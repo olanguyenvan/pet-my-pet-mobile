155323650ed15a854fe6c7cfabb65069ddce2956
Tomasz Błachut <blachut.tomasz@gmail.com>
2018-05-12 15:50:08 +0200

    Organize navigation

--- navigation/MainTabNavigator.tsx
+++ navigation/MainTabNavigator.tsx
@@ -2,37 +2,11 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator, createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
-
 import TabBarIcon from '../components/TabBarIcon';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import AddPetForm from "../screens/manage-profile/AddPetForm";
-import DetailsScreen from '../screens/browse/Details';
-import CareRequestListScreen from '../screens/browse/CareRequestListScreen';
-import HostOfferListScreen from '../screens/browse/HostOfferListScreen';
-
-
-const ListTopNavigator = createMaterialTopTabNavigator({
-  HostOffers: HostOfferListScreen,
-  CareRequests: CareRequestListScreen,
-});
-
-ListTopNavigator.navigationOptions = {
-  headerStyle: {height: 0},
-};
-
-const BrowseStack = createStackNavigator({
-  Home: ListTopNavigator,
-  Details: DetailsScreen,
-});
-
-BrowseStack.navigationOptions = {
-  title: 'Lol',
-  tabBarLabel: 'Browse',
-  tabBarIcon: ({focused}) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? `ios-list` : 'md-list'}/>
-  ),
-};
+import AddPetForm from '../screens/manage-profile/AddPetForm';
+import BrowseStack from './BrowseStack';
 
 const LinksStack = createStackNavigator({
   Links: LinksScreen,
